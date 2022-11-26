e290f7605f9c1ca68caf17ec3c9fd483fdd1bef7
Bipin <xperiabpn@gmail.com>
2022-11-26 19:04:26 +0545

    Escape Sequence in JS

---
+++ escaping.js
@@ -0,0 +1,23 @@
+/*
+	ESCAPE SEQUENCES IN JS
+	Escape characters are used mostly with strings. Strings are included inside single quote(''), double quote("") or backtick (``). Escape sequences are written as \(escape_character). E.g. \n
+
+	Some escape sequences used in JS are 
+	1. \b			Backspace
+	2. \f 			Form Feed
+	3. \n			New Line
+	4. \r			Carriage Return
+	5. \t			Horizontal Tabulator (Tab)
+	6. \v			Vertical Tabulator
+*/
+
+console.log("Use of \b           'b' escape sequence.");
+console.log("Use of \f 'f' escape sequence.");
+console.log("Use of \n 'n' escape sequence ");
+console.log("Use of \r 'r'.");
+console.log("use of \t 't'.");
+console.log("Use of \v 'v'");
+
+
+//For actual backslash
+console.log("The newline character is written as '\\n'");
