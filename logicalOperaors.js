221a36c465c5e960ff757655adb61289cb0b1c00
Bipin <xperiabpn@gmail.com>
2022-11-30 16:35:27 +0545

    Logical Operators

---
+++ logicalOperaors.js
@@ -0,0 +1,17 @@
+//Logical Operators (AND, OR and NOT)
+
+//1. AND Operator
+console.log(true && false);	//false
+console.log(true && true);	//true
+
+
+//2. OR Operator
+console.log(false || true);
+console.log(false || false);
+
+
+//3. NOT Operator
+console.log(!true);
+console.log(!false);
+
+
