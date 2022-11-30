f93a3b5cd8fb3ba14d72c46e11180770b679cdc6
Bipin <xperiabpn@gmail.com>
2022-11-30 16:29:42 +0545

    Unary Operator

---
+++ unaryOperator.js
@@ -0,0 +1,33 @@
+//Unary Operators
+
+//1. typeof operator
+console.log(typeof("Ar"));	//string
+console.log(typeof(33));		//number
+console.log(typeof(true));	//boolean
+console.log(typeof (null));	//object
+console.log(typeof(x));		//undefined
+
+
+//2. Boolean Values (Only two possible values are there: True or False)
+//Example
+console.log(3 > 2);	//true
+console.log(3<2);	//false
+
+//Strings can also be compared like numbers 
+//Example 
+console.log("Aruna" < "Bipin");	//Generally, alphabetically, strings are compared but there are other specific rules also for this.
+//Non alphabetic characters can also be compared
+
+
+
+console.log("Itchy" != "Scratchy");
+console.log("Apple" == "Orange");
+
+
+
+
+
+//3. NaN (Not a Number)
+//NaN is that type of value in JS which is not equal to itself
+console.log(NaN == NaN);
+
