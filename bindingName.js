fb3773bd32ad280bc5b5d093db35fe138dce9c4a
Bipin <xperiabpn@gmail.com>
2022-12-01 18:55:27 +0545

    Rules for Binding Name

---
+++ bindingName.js
@@ -0,0 +1,11 @@
+let $catch="Catch";	//valid
+console.log($catch);
+
+let _catch_me= "Catch me if you can";
+console.log(_catch_me);	//valid
+
+let #val=23;	//invalid
+
+let catch22= true; //valid
+
+let don= 'DOM';	//valid
