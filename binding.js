dbf12db349f5765e99272da0e3abedded1ae4200
Bipin <xperiabpn@gmail.com>
2022-12-01 17:58:13 +0545

    Binding

---
+++ binding.js
@@ -0,0 +1,12 @@
+//Binding simply means making the program remember our required values whenever necessary.
+
+let caught= 5*5;
+console.log(caught);
+
+//Binding values can be changed according to the need of the program
+//Program to remember the remaining dept of a friend.
+
+let dept=150;
+//suppose that friend paid Rs.35 then the remaining dept is
+dept=dept-35;
+console.log("Rs." + dept);
