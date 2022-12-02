03bc2eaf83e344e305c26ebd862b88b09f83b4dd
Bipin <xperiabpn@gmail.com>
2022-12-02 18:55:02 +0545

    Control Flow

---
+++ controlFlowExample.js
@@ -0,0 +1,4 @@
+//To get a number through prompt and print whose square root is that number
+
+let num=Number( prompt("Enter a number"));
+document.write("Your number is the square root of " ,num*num);
