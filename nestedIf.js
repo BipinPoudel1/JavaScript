6e70455256a7275249193a80780bdf5b3960e046
Bipin <xperiabpn@gmail.com>
2022-12-03 19:48:26 +0545

    Nested If- else condition

---
+++ nestedIf.js
@@ -0,0 +1,8 @@
+let num=Number(prompt("Enter any number"));
+if(num<10){
+	document.write("The number is small.")
+}else if(num<100){
+	document.write("The number is medium.")
+}else{
+	document.write("The number is quite large.")
+}
