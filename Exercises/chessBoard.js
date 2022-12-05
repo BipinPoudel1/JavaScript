859fe6325efabe8884733b18526d4e20d2ae09c0
Bipin <xperiabpn@gmail.com>
2022-12-05 22:35:36 +0545

    Exercise 1 Qstn 3 (Chessboard)

---
+++ Exercises/chessBoard.js
@@ -0,0 +1,28 @@
+/*
+Write a program that creates a string that represents an 8×8 grid, using
+newline characters to separate lines. At each position of the grid there
+is either a space or a "#" character. The characters should form a
+chessboard.
+Passing this string to console.log should show something like this:
+       #  #  #  #
+     #  #  #  #
+      #  #  #  #
+     #  #  #  #
+      #  #  #  #
+     #  #  #  #
+      #  #  #  #
+     #  #  #  #
+
+*/
+
+
+for(let i=1; i<=8; i++){
+      for(let j=1; j<=8; j++){
+            if((i+j)%2==0){
+                  process.stdout.write(" ");
+            }else{
+                  process.stdout.write("#");
+            }
+      }
+      process.stdout.write("\n");
+}
