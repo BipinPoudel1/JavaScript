a66d386a7ed74afdbbdec74d4332d44b94321e3b
Bipin <xperiabpn@gmail.com>
2022-12-05 19:52:24 +0545

    Triangle in JS

---
+++ Exercises/triangle.js
@@ -0,0 +1,14 @@
+console.log("Exercise 1");
+
+let i, j;
+
+for(i=1; i<=7; i++){
+      for(j=1; j<=i; j++){
+            process.stdout.write("# ");
+      }
+      process.stdout.write("\n");
+}
+
+/*
+      Here, process.stdout.write is alternative for console.log or document.write. Those things print the each iteration in new line where program like this (pattern) becomes very non- interactive. So, to print more iterations on the same line, we use process.stdout.write.
+*/
