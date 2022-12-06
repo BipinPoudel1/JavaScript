12a19cd4efa4e0b9230c17c2c0e7aa4a4e05a1ed
Bipin <xperiabpn@gmail.com>
2022-12-06 21:03:21 +0545

    Functions

---
+++ function2.js
@@ -0,0 +1,10 @@
+console.log("The future says: ", future());
+
+function future(){
+      return "You'll never have flying cars.";
+}
+
+
+/*
+      Here, we first call the function and then define it. It works!!! :)
+*/
---
+++ shortFunction.js
@@ -0,0 +1,5 @@
+function square(x){
+      return x**2;
+}
+
+console.log(square(2));
