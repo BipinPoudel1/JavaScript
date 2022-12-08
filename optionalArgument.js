a968a61dfe210e182fbf92f0e2b9f9fe7034d69c
Bipin <xperiabpn@gmail.com>
2022-12-08 17:04:50 +0545

    Optional Arguments

---
+++ optionalArgument.js
@@ -0,0 +1,2 @@
+function square(x){return x*x}
+console.log(square(4, true, "hedgehog")); //16... It ignores the extra arguments
---
+++ optionalArguments1.js
@@ -0,0 +1,10 @@
+function minus(a, b){
+      if(b===undefined){
+            return -a;
+      }else{
+            return a-b;
+      }
+}
+
+console.log(minus(10));
+console.log(minus(10, 5));
