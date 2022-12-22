2aa41356694d9c3cbbf5af2073bc493d3fca6b0a
Bipin <xperiabpn@gmail.com>
2022-12-22 17:09:10 +0545

    Power using recursion

---
+++ power.js
@@ -0,0 +1,10 @@
+//Recursive function to calculate power of given base
+function power(base, exponent){
+	if(exponent==0){
+		return 1;
+	}else{
+		return base*power(base, exponent-1);
+	}
+}
+
+console.log(power(2, 3));
