82bcbfe672ef76565fd8fe95b2fb25161bc07c24
Bipin <xperiabpn@gmail.com>
2023-01-05 19:17:58 +0545

    String Length Calculation

--- Exercises/strLength.js
+++ Exercises/strLength.js
@@ -1,2 +1,4 @@
-let str="Just one small positive thought in the morning can change your whole day."
-console.log(str.length);      //variable.length returns the length of the string contained by that variable
+let str="Life itself is the most wonderful fairy tale."
+console.log("Your string is: ");
+console.log(str);
+console.log("The length of this string is: "+str.length);      //variable.length returns the length of the string contained by that variable
